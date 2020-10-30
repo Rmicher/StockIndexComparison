6af273a59d4a0962eba6f8ff3c64fdd159adf28d
Kyle Ceffaratti <70409237+kceffaratti3@users.noreply.github.com>
2020-10-30 00:21:53 -0400

    I merged what I could

--- stock-picker-app/lib/js/main.js
+++ stock-picker-app/lib/js/main.js
@@ -1,15 +1,15 @@
 // TODO: stockDetails needs replaced with API data
 var stockDetails = [{"ticker": "AAPL", "price": "115.04"}, {"ticker": "GOOG", "price": "1641.00"}, {"ticker": "FB", "price": "284.79"}];
 
-// Append the stock options to the dropdown
-var dropdown = document.getElementById("selectStock");
-for (var i = 0; i < stockDetails.length; i++) {
-  var stock = stockDetails[i].ticker;
-  var option = document.createElement("option");
-  option.textContent = stock;
-  option.value = stock;
-  dropdown.appendChild(option);
-}
+// // Append the stock options to the dropdown
+// var dropdown = document.getElementById("selectStock");
+// for (var i = 0; i < stockDetails.length; i++) {
+//   var stock = stockDetails[i].ticker;
+//   var option = document.createElement("option");
+//   option.textContent = stock;
+//   option.value = stock;
+//   dropdown.appendChild(option);
+// }
 
 // Event listener for the dropdown
 dropdown.addEventListener("change", onSelectChange);
@@ -28,21 +28,29 @@ function showStockDetails(selectedStock){
 }
 
 // Add stock to radar graph and to bottom row of grid when button is clicked
-document.getElementById("AddToPortfolioButton").onclick = onClickAddToPortfolioButton;
-var stockNumber = 0;
+// document.getElementById("AddToPortfolioButton").onclick = onClickAddToPortfolioButton;
+// var stockNumber = 0;
 var listOfSelectedStocks = [];
 function onClickAddToPortfolioButton(){
-  var value = dropdown.value;
-  listOfSelectedStocks.push(value)
+  selectValue = d3.select('select').property('value');
+  listOfSelectedStocks.push(selectValue)
+  
+  listOfSelectedStocks = listOfSelectedStocks.slice(Math.max(listOfSelectedStocks.length - 5, 0))
+  var selectedObject = Object({'Tickers':listOfSelectedStocks})
   AddStockDetailsGrid(value, stockNumber++);
   // TODO: createRadarGraph()
   // TODO: findSimilarIndexes()
 }
 
 // Creates a stock details grid member and refreshes the radar graph with the new selected stock attributes
-const stockNumberArray = ["stock0", "stock1", "stock2", "stock3", "stock4"]
-function AddStockDetailsGrid(selectedStock, stockNumber){
-  var stock = document.getElementById(stockNumberArray[stockNumber]);
+
+function AddStockDetailsGrid(selectedStock, stockNumber,color, selectedObject){
+  const stockNumberArray = ["#stock0", "#stock1", "#stock2", "#stock3", "#stock4"]
+  // debugger;
+  var stock = d3.select(stockNumberArray[stockNumber]);
   stock.style.visibility = "visible";
   stock.innerHTML = selectedStock;
+  // debugger;
+  stock.style('fill',color(stockNumber)).text(selectedObject.Tickers[stockNumber]);
+  // stock.select('text').text(selectedStock)
 }
